370d1ca676ba94a54bf3bc67dadfac04d7abc2f0
Nitish Kumar Singh <nitishkumar910209@gmail.com>
2026-07-07 22:28:00 +0530

    Update message_placeholder.py

--- message_placeholder.py
+++ message_placeholder.py
@@ -2,8 +2,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 chat_template = ChatPromptTemplate([
     ('system','You are a helpful customer support agent'),
     MessagesPlaceholder(variable_name='chat_history'),
-    ('human','{query}')
-])
+    ('human','{query}')])
 
 chat_history = []
 with open('chat_history.txt') as f:
